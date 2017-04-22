8a69d603db28bda8513b7e70bf88823ecbfb41a8
Tancredi Trugenberger <tancredi.trugenberger@gmail.com>
2017-04-22 19:46:31 +0200

    Default port'

--- server/index.js
+++ server/index.js
@@ -52,6 +52,8 @@ io.on('connection', (socket) => {
     });
 });
 
-server.listen(3000, () => {
-    console.log('listening on *:3000');
+let port = process.env.PORT || 3000;
+
+server.listen(port, () => {
+    console.log(`listening on *:${ port }`);
 });
